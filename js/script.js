4fc7b4a419c0704179add43849fd904ed4db7ad9
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2026-06-05 08:47:45 +0300

    loop basics application

--- js/script.js
+++ js/script.js
@@ -40,3 +40,27 @@ function formatTime(i) {
     if (i < 10) { i = "0" + i }  // Add leading zero to minutes and seconds if they are less than 10
     return i;
 }
+
+function daysInMonth(TimeDate) {
+    // calculate the number of days in the current month
+    var currentMonth = TimeDate.getMonth() + 1; // Get the current month (0-11)
+    var months_with_30_days = [4, 6, 9, 11]; // April, June, September, November
+    var months_with_31_days = [1, 3, 5, 7, 8, 10, 12]; // January, March, May, July, August, October, December
+    var february_month = 2; // February
+    if (months_with_30_days.includes(currentMonth)) {
+        var daysInMonth = 30;
+    } else if (months_with_31_days.includes(currentMonth)) {
+        var daysInMonth = 31;
+    } else if (currentMonth === february_month) {
+        // Check for leap year
+        var year = TimeDate.getFullYear();
+        if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+            var daysInMonth = 29; // Leap year
+        } else {
+            var daysInMonth = 28; // Non-leap year
+        }
+    } else {
+        var daysInMonth = 0; // Default case, should not occur
+    }
+    return daysInMonth; // Return the number of days in the current month
+}
